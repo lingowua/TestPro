7bdfa4dc92bbcb75f40a3eaad2c471d1d6501207
lingowu <lingowu@tencent.com>
2022-01-10 16:34:03 +0800

    新建测试工程

---
+++ TestPro/TestPro/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestPro
+//
+//  Created by lingowu on 2022/1/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestPro/TestPro/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestPro
+//
+//  Created by lingowu on 2022/1/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
